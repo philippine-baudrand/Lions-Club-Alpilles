6a26bdc888eec899f08b5f060955ce59034e7d9e
philippine-baudrand <philippine.baudrand@gmail.com>
2020-11-06 18:33:28 +0100

    bug

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,42 +4,42 @@ import './header.scss';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div>
-    <h1> Header </h1>
-    <ul className="bloc-navbar">
+  <div className="header-lions">
+
+    <ul className="bloc-navbar-lions">
       <span>
       <Link className="mainLogo" to="/home">
-        <button className="navbar">Accueil</button>
+        <button className="navbar-lions">Accueil</button>
         </Link>
       </span>
 
       <span>
       <Link className="mainLogo" to="/club">
-        <button className="navbar">Notre Club</button>
+        <button className="navbar-lions">Notre Club</button>
         </Link>
       </span>
 
       <span>
       <Link className="mainLogo" to="/evt">
-        <button className="navbar">évènements</button>
+        <button className="navbar-lions">évènements</button>
         </Link>
       </span>
 
       <span>
       <Link className="mainLogo" to="/members">
-        <button className="navbar">Membres</button>
+        <button className="navbar-lions">Membres</button>
         </Link>
       </span>
 
       <span>
       <Link className="mainLogo" to="/photo">
-        <button className="navbar">Photos</button>
+        <button className="navbar-lions">Photos</button>
         </Link>
       </span>
 
       <span>
       <Link className="mainLogo" to="/contact">
-        <button className="navbar">Contact</button>
+        <button className="navbar-lions">Contact</button>
         </Link>
       </span>
     </ul>
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,23 +1,62 @@
 // == Import npm
 import React from 'react';
 import './home.scss';
+import Carousel from 'react-bootstrap/Carousel';
 import Footer from '../Footer/footer';
-import Header from '../Header/header';
 import photo from './lions-woman.jpg';
-
+import photo2 from './foire-gras-20.jpg';
+import Header from '../Header/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => (
   <div>
 
     <Header />
-      <div className="lions-presentation">
-        <span className="text-presentation">
+    <div>
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src={photo2}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+    <div className="lions-presentation">
+      <span className="text-presentation">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget quam blandit, semper urna accumsan, scelerisque leo. Mauris a ex et eros gravida sodales ultricies sed nunc. Donec consequat in eros ac rhoncus. Phasellus tempus, ipsum id lobortis tempor, dolor tortor viverra erat, nec vehicula est odio sit amet mauris. Cras et faucibus metus, id fermentum sem. Donec varius congue nulla non pretium. Cras vitae ante pharetra, pellentesque est eu, porta neque. Curabitur ultrices odio volutpat, euismod urna sit amet, scelerisque purus.
-        </span>
-        <span className="picture-presentation">
+      </span>
+      <span className="picture-presentation">
         <img className="picture" src={photo} alt="lions-woman" />
-        </span>
-      </div>
+      </span>
+    </div>
     <Footer />
   </div>
 );
