95b5e4703bcd9403de36b69e6a08f3b952bea83a
unknown <mrl.remi@gmail.com>
2020-11-09 17:05:13 +0100

    adding fake data for test

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        name: ' Jean-Michel',
+        photo: '',
+        fonction: ' Présidente  ',
+    },
+
+    {
+        name: ' Pauline',
+        photo: '',
+        fonction: ' trésorier ',
+    },
+
+    {
+        name: ' Djewely',
+        photo: '',
+        fonction: ' comptable ',
+    },
+
+    {
+        name: ' Pierre',
+        photo: '',
+        fonction: ' secrétaire',
+    },
+
+    {
+        name: ' Paul',
+        photo: '',
+        fonction: 'vice président',
+    },
+
+    {
+        name: ' Jacques',
+        photo: '',
+        fonction: 'bénévole',
+    },
+];
