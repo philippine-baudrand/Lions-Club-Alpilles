299b2d11ea9faffbf90e9b679d1a616e14de3858
Philou <Philou@iMac-de-Philou.local>
2020-11-13 13:10:51 +0100

    work on club page

---
+++ src/data/data-club.js
@@ -0,0 +1,6 @@
+export default [
+    {  
+        name: ' Club Féminin Paradou les Alpilles<',
+        text: "Le Lions Club PARADOU LES ALPILLES regroupent dans la convivialité et l'esprit de bonne camaraderie 29 FEMMES autour des valeurs humanistes du LIONISME",
+    },
+];
