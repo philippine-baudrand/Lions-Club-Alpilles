74cfb5559ed63faf11d103cfd6054409cb7718b9
philippine-baudrand <philippine.baudrand@gmail.com>
2020-11-17 10:17:01 +0100

    correct button size to go back on event page

--- src/components/Members/members.js
+++ src/components/Members/members.js
@@ -7,25 +7,25 @@ import Header from '../Header/header';
 const Members = ( props ) => {
   const { list } = props;
     return (
-    <div className="member">
-    <Header />  
-    <h1 className="member_title"> Members </h1>
-      <div className="card">
-        {
-          list.map((cardObject) =>
-          <div className="card_unit">
-            <h3 className="card_name">{cardObject.name}</h3>
-            <div >
-              <img  
-              src={cardObject.photo}
-              className="card_img"/>  
-            </div>
-            <p className="card_function"> {cardObject.fonction} </p>
-          </div>
-          )
-        }
-      </div>
-    <Footer />
+      <div className="member">
+        <Header />  
+        <h1 className="member_title"> Members </h1>
+          <div className="pictureList">
+            {
+              list.map((cardObject) =>
+              <div className="card_unit">
+                <h3 className="card_name">{cardObject.name}</h3>
+                <div >
+                  <img  
+                  src={cardObject.photo}
+                  className="card_img"/>  
+                </div>
+                <p className="card_function"> {cardObject.fonction} </p>
+              </div>
+              )
+            }
+        </div>
+      <Footer />
   </div>
   )};
 export default Members;
