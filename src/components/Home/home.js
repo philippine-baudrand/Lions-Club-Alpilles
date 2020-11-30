bd1e2aeb58341b9959c00bd82a8a13a91b97ca34
philippine-baudrand <philippine.baudrand@gmail.com>
2020-11-30 16:58:56 +0100

    change css form in contact

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -16,14 +16,13 @@ const Home = () => (
     <div className="lions-presentation">
       <div className="picture-presentation">
         <div className="lions-paradou">
-        <img className="picture" src={photo} alt="lions-woman" />
-        <img className="picture" src="https://media.discordapp.net/attachments/739121254438666292/782973427295584256/logo_paradou.jpg" alt="lions-woman" />
+          <img className="picture" src={photo} alt="lions-woman" />
+          <img className="picture" src="https://media.discordapp.net/attachments/739121254438666292/782973427295584256/logo_paradou.jpg" alt="lions-woman" />
         </div>
         <h2>Convivialité et humanisme, 29 femmes lionistes</h2>
       </div>
       <div className="cards-presentation">
         <div className="cards">
-
         <Link to="/foire-au-gras"><h3 className="cards-title">La foire gourmande au Gras</h3>
           <div className="cards-img">
             <img
@@ -34,21 +33,20 @@ const Home = () => (
             
           </div>
 
-
-            <p className="cards-text">Cette année, en raison de la crise sanitaire, la Foire Gourmande au Gras de Fontvieille est annulée dans son format habituel.</p>
+          <p className="cards-text">Cette année, en raison de la crise sanitaire, la Foire Gourmande au Gras de Fontvieille est annulée dans son format habituel.</p></Link>
         </div>
         <div className="cards">
-          <Link to="/vide-commode"><h3 className="cards-title">Le vide commode d'Arlesiennes</h3></Link>
+        <Link to="/vide-commode"><h3 className="cards-title">Le vide commode d'Arlesiennes</h3>
           <div className="cards-img">
             <img
               src="https://www.provence-camargue-tourisme.com/image/5246-normal.jpg" alt="foire-au-gras"
               className="cards-img"
             />
           </div>
-          <p className="cards-text">Un rdv incontournable pour ceux qui sont à l’affût de l’article manquant à leur costume ou pour la confection de celui-ci. Organisé par le Lions club Paradou les Alpilles. Tombola, à gagner : un Ruban d’arlésienne.</p>
+          <p className="cards-text">text Arlesiennes</p></Link>
         </div>
         <div className="cards">
-          <Link to="/telethon"><h3 className="cards-title">Téléthon</h3></Link>
+        <Link to="/telethon"><h3 className="cards-title">Téléthon</h3>
           <div className="cards-img">
             <img
               src="https://cdn.discordapp.com/attachments/778173311783403560/781450894112980992/13864739.jpg"
@@ -56,14 +54,14 @@ const Home = () => (
               className="cards-img"
             />
           </div>
-          <p className="cards-text">Le Téléthon rassemble 5 millions de personnes. Depuis 1987, il donne à l'association l’AFM-Téléthon les moyens de poursuivre son combat contre la maladie.</p>
+          <p className="cards-text">Le Téléthon rassemble 5 millions de personnes. Depuis 1987, il donne à l'association l’AFM-Téléthon les moyens de poursuivre son combat contre la maladie.</p></Link>
         </div>
         <div className="cards">
-          <Link to="/salon-créateurs"><h3 className="cards-title">Le salon des créateurs</h3></Link>
+        <Link to="/salon-créateurs"><h3 className="cards-title">Le salon des créateurs</h3>
           <div className="cards-img">
             <img></img>
           </div>
-          <p className="cards-text">text Le salon des créateurs</p>
+          <p className="cards-text">text Le salon des créateurs</p></Link>
         </div>
       </div>
     </div>
